ce170788124febf5393dbe29bcdef56c4779108b
abbos <abbosvaliev2003@gmail.com>
2022-10-29 21:10:30 +0500

    feat: add new modal

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -210,3 +210,44 @@ setTime('timer', endtime);
 // The timer has been redesigned! Look for it here
 // Таймер переработан! Ищите его здесь
 // https://codepen.io/mosmain/pen/LYQGdwR
+
+
+
+
+
+
+
+
+
+
+
+// Модальное окно
+function bindModal(trigger, modal, close) {
+    trigger = document.querySelector(trigger),
+    modal = document.querySelector(modal),
+    close = document.querySelector(close)
+
+    const body = document.body
+
+    trigger.addEventListener('click', e => {
+        e.preventDefault()
+        modal.style.display = 'flex'
+        body.classList.add('locked')
+    });
+    close.addEventListener('click', () => {
+        modal.style.display = 'none'
+        body.classList.remove('locked')
+    });
+    modal.addEventListener('click', e => {
+        if (e.target === modal) {
+        modal.style.display = 'none'
+        body.classList.remove('locked')
+        }
+    })
+}
+
+// ПЕРВЫЙ аргумент - класс кнопки, при клике на которую будет открываться модальное окно.
+// ВТОРОЙ аргумент - класс самого модального окна.
+// ТРЕТИЙ аргумент - класс кнопки, при клике на которую будет закрываться модальное окно.
+bindModal('.modal__btn', '.modal__wrapper', '.modal__close')
+
